ca760bd71a284aaed6f497321b64a99a1774e265
Paul Ingles <paul@oobaloo.co.uk>
2010-10-12 14:36:10 +0100

    Add code + notes from ex1

--- ex1.clj
+++ ex1.clj
@@ -52,3 +52,80 @@
 
 ((if true - +) 1 2)
 
+(defn good-enough?
+  [guess x]
+  (< (Math/abs
+      (- (square guess)
+         x))
+     0.001))
+
+;;Exercise 1.6
+;;It's to do with the application of operands (applicative order),
+;;not with how new-if returns results of the expressions. The example (new-if (= 2 3) 0 5
+;;works because the operands are atoms. With compound expressions, the
+;;interpreter has to expand the combination (irrespective of which
+;;expression it returns the result of) causing it to enter an
+;;infinite loop.
+
+
+;;Exercise 1.7
+;;
+;;It doesn't work with small numbers because the margin of error is
+;;only 0.001. With large numbers, there isn't enough precision for the
+;;good enough test to ever pass (and thus would iterate forever). e.g:
+;; 
+;; sqrt 2,000,000,000,000
+;;
+;;it stops refining answer after iteration 26, which, is still
+;;outside of an acceptable range for good-enough?: there isn't enough
+;;precision to continue to refine, causing an infinite loop.
+;; 
+;; Rewrite good-enough + collect change between interations
+
+(defn good-enough?
+  [guess prev-guess]
+  (< (Math/abs (- prev-guess
+                  guess))
+     0.001))
+
+(defn average
+  [x y]
+  (/ (+ x
+        y)
+     2))
+
+(defn improve
+  [guess x]
+  (average guess
+           (/ x
+              guess)))
+
+(defn sqrt-iter
+  [guess x prev-guess]
+  (if (good-enough? guess
+                    prev-guess)
+    guess
+    (recur (improve guess x) x guess)))
+
+(defn sqrt
+  [x]
+  (sqrt-iter 1.0 x 0))
+
+(defn improve-cube
+  [guess x]
+  (/ (+ (/ x (square guess))
+        (* 2 guess))
+     3))
+
+(defn cuberoot-iter
+  [guess x prev-guess]
+  (if (good-enough? guess
+                    prev-guess)
+    guess
+    (recur (improve-cube guess x) x guess)))
+
+(defn cuberoot
+  [x]
+  (cuberoot-iter 1.0 x 0))
+
+
