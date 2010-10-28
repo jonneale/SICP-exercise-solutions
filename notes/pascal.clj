83f4d0e156ab0048c0b95253df3f443536c2a1b5
Jon Neale <jon.neale@uswitch.com>
2010-10-28 09:55:17 +0100

    Added some notes

---
+++ notes/pascal.clj
@@ -0,0 +1,28 @@
+(defn pas
+  [height width]
+  (cond 
+    (= width 0) 
+        1
+    (or (< width 0) (< height 0))
+        0
+    (> width height)
+        0
+    :else
+        (+ (pas (- height 1) (- width 1)) 
+           (pas (- height 1) width))))
+  
+(defn print-pas-iter
+  [height width max-height current-line]
+  (cond 
+    (> height max-height)
+      (println current-line)
+    (> width height)
+      (do 
+        (println current-line)
+        (print-pas-iter (+ height 1) 0 max-height ()))
+    :else
+      (print-pas-iter height (+ 1 width) max-height (conj current-line (pas height width)))))
+            
+(defn print-pas
+  [depth]
+  (print-pas-iter 0 0 depth ()))
