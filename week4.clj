fa187f96c81ec395d9305d24594b79835a0279ae
Jon Neale <jon.neale@uswitch.com>
2010-10-30 16:23:31 +0100

    Added solutions to exercises 1.17 and 1.18

--- week4.clj
+++ week4.clj
@@ -1,4 +1,4 @@
-;; exp 1.16, write an iterative expt function
+;; ex 1.16, write an iterative expt function
 (defn fast-expt 
       [b n]
       (defn square
@@ -23,3 +23,46 @@
                   (even? n) (iter-expt (square b) (/ n 2) product)
                   :else (iter-expt b (- n 1) (* b product)))))
       (iter-expt b n 1))
+
+;; ex 1.17
+(defn times
+      [a b]
+      (if (= b 0)
+          0
+          (+ a (times a (- b 1)))))
+
+(defn fast-times
+      [a b]
+      (defn double
+            [x]
+            (* x 2))
+      (defn halve
+            [x]
+            (/ x 2))
+      (cond 
+            (= b 1)
+            a
+            (even? b)
+            (fast-times (double a) (halve b))
+            :else
+            (+ a (fast-times a (- b 1)))))
+
+;; ex 1.18
+(defn faster-times
+      [a b]
+      (defn double
+            [x]
+            (* x 2))
+      (defn halve
+            [x]
+            (/ x 2))
+      (defn times-iter
+            [a b sum]
+            (cond
+                (= b 0)
+                sum
+                (even? b)
+                (times-iter (double a) (halve b) sum)
+                :else
+                (times-iter a (- b 1) (+ a sum))))
+      (times-iter a b 0))
