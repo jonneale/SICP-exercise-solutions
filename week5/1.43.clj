dbf165e05b4ef366f08217471c6a780bf01b3f00
Jon Neale <jon.neale@uswitch.com>
2011-01-11 13:41:42 +0000

    Fixed 1.43

--- week5/1.43.clj
+++ week5/1.43.clj
@@ -2,6 +2,20 @@
   [f g]
   (fn[x] (f (g x))))
   
+(defn repeated-iter
+  [f g x i]
+  (cond (= x i)
+          f
+        :else
+        (repeated-iter (compose f g) g x (+ i 1))))
+        
 (defn repeated
   [f x]
-  (fn[times] compose  ))
+  (repeated-iter f f x 1))
+  
+  ; user=> ((repeated inc 4) 1)
+  ; 5
+  ; user=> ((repeated inc 6) 1)
+  ; 7
+  ; user=> ((repeated inc 8) 1)
+  ; 9
