fe01f627e6f846f2daf27209b354201d822a5de7
Jon Neale <jon.neale@uswitch.com>
2011-01-24 19:00:28 +0000

    Generated a leiningen project ehre and added 2.2 solution

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject sicp-examples "1.0.0-SNAPSHOT"
+  :description ""
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
---
+++ test/sicp_examples/test/core.clj
@@ -0,0 +1,6 @@
+(ns sicp-examples.test.core
+  (:use [sicp-examples.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
