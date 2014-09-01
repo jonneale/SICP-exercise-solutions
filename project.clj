99e95f55ab7608c06941c9f1ac95c2db494f0ad5
jonneale <jonneale@gmail.com>
2014-09-01 08:56:00 +0100

    Upgrade clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject sicp-examples "1.0.0-SNAPSHOT"
-  :description ""
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+(defproject sicp-examples "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :jvm-opts ["-Xmx4g"])
